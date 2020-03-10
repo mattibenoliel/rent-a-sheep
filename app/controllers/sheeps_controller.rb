160530e7ee5a71980ce8c4261ea22478cbb3c9c9
vanmak <vanmakdavid.lwg@gmail.com>
2020-03-10 14:16:34 +0100

    Edit update ressources

--- app/controllers/sheeps_controller.rb
+++ app/controllers/sheeps_controller.rb
@@ -1,4 +1,18 @@
 class SheepsController < ApplicationController
   def index
   end
+
+  def edit
+   @sheep = Sheep.find(params[:id])
+ end
+
+
+ def update
+     if @restaurant.update(sheep_params)
+       redirect_to sheep_path(@sheep)
+     else
+       render :edit
+     end
+ end
+
 end
