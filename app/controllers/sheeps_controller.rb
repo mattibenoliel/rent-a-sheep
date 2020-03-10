ceda7f95431570b6238442ea3057673fc1d81d16
Matti Benoliel <benolielmatti@hotmail.fr>
2020-03-10 12:35:30 +0100

    create index show destroy

--- app/controllers/sheeps_controller.rb
+++ app/controllers/sheeps_controller.rb
@@ -1,4 +1,21 @@
 class SheepsController < ApplicationController
+  before_action :set_sheep, only: [:show, :destroy]
   def index
+    @sheeps = Sheep.all
   end
+
+  def show;  end
+
+  def destroy
+    @sheep.destroy
+
+    redirect_to sheeps_path
+  end
+
+  private
+
+  def set_sheep
+    @sheep = Sheep.find(params[:id])
+  end
+
 end
