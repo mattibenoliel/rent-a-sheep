3f91b9e2ca66aac4d50a062832009b3bb7fccdb7
Matti Benoliel <benolielmatti@hotmail.fr>
2020-03-11 12:13:51 +0100

    create dashboard column user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,9 @@ class PagesController < ApplicationController
 
   def home
   end
-  
-  def dashboard
 
+  def dashboard
+    @user_sheeps = current_user.sheeps
+    @user_bookings = current_user.bookings
   end
 end
