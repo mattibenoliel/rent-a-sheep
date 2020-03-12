2825d1a4f738152cb61f206cf9585672362a0a8c
Matti Benoliel <benolielmatti@hotmail.fr>
2020-03-12 15:29:46 +0100

    pg search

--- app/models/sheep.rb
+++ app/models/sheep.rb
@@ -2,4 +2,14 @@ class Sheep < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   has_many :bookings, dependent: :destroy
+
+  include PgSearch::Model
+   pg_search_scope :global_search,
+     against: [],
+     associated_against: {
+      user: [ :address ]
+      },
+     using: {
+       tsearch: { prefix: true } # <-- now `superman batm` will return something!
+     }
 end
