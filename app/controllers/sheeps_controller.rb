85314d2a7663b45fa68bc4a67b6c30c3782b63e5
Celia Franceschi <franceschi.ce@gmail.com>
2020-03-12 18:04:12 +0100

    working on marker style

--- app/controllers/sheeps_controller.rb
+++ app/controllers/sheeps_controller.rb
@@ -15,11 +15,11 @@ class SheepsController < ApplicationController
     end
 
     @sheeps = @sheeps.geocoded
-    @markers = @sheeps.map do |sheep|
+    @markers = @sheeps.group_by(&:latitude).map do |latitude,sheeps|
       {
-        lat: sheep.latitude,
-        lng: sheep.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { sheep: sheep })
+        lat: sheeps.first.latitude,
+        lng: sheeps.first.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { sheeps: sheeps })
       }
     end
 
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -13,7 +13,14 @@ const buildMap = () => {
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-    new mapboxgl.Marker()
+
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.width = '25px';
+    element.style.height = '25px';
+    element.backgroundColor = '#293F14';
+
+    new mapboxgl.Marker(element)
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup)
       .addTo(map);
