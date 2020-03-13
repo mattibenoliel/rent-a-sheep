50e5896fc6adce84cb056a8cd4107115172d07af
Matti Benoliel <benolielmatti@hotmail.fr>
2020-03-13 16:27:04 +0100

    booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,13 @@
 class Booking < ApplicationRecord
   belongs_to :sheep
   belongs_to :user
+  validate :booking_validity
+
+  private
+
+  def booking_validity
+    if sheep.bookings.present?
+      errors.add(:end_date, "Already booked")
+    end
+  end
 end
