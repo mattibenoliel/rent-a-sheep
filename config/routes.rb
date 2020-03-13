a307a0432ee87a7c4f2690cfda8de70c7f80c02d
Patrick Blanc <perso@patrickblanc.net>
2020-03-13 10:36:51 +0100

    who page begin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
+  get 'who', to: 'pages#who'
   resources :sheeps do
       resources :bookings, only: [:new, :create]
     end
